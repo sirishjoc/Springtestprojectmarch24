ae91c736789e7c42267c96e9328a932f65d01b80
sirishjoshi <sirish@fusemachines.com>
2018-09-19 12:00:47 +0545

    running mongodb with less dependency

--- src/main/java/com/sirish/testspring/config/AppConfig.java
+++
@@ -1,9 +0,0 @@
-//package com.sirish.testspring.config;
-//
-//import org.springframework.context.annotation.ComponentScan;
-//
-//@ComponentScan(basePackages = "com.sirish.testspring")
-//public class AppConfig {
-//	
-//	
-//}
